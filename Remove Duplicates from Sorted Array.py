21a26d011d189d8f2504a9e319bb4f90cbf11f85
bru <38256396+brahada@users.noreply.github.com>
2020-06-02 02:03:27 +0530

    Create Remove Duplicates from Sorted Array.py

---
+++ Remove Duplicates from Sorted Array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeDuplicates(self, nums):
+        if not nums:
+            return 
+        slow = fast = 0
+        while fast <= len(nums) - 1:
+            if nums[fast] != nums[slow]:
+                nums[slow+1] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow + 1
